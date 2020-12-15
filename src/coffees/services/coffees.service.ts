a879783803f7a90393d750b88ca7765895766d84
tabarino <tabarino@outlook.com>
2020-12-15 23:17:55 +0000

    Send User-Friendly Messages

--- src/coffees/services/coffees.service.ts
+++ src/coffees/services/coffees.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Coffee } from '../entities/coffee.entity';
 
 @Injectable()
@@ -15,7 +15,12 @@ export class CoffeesService {
     }
 
     findOne(id: string) {
-        return this.coffees.find(item => item.id === +id);
+        const coffee = this.coffees.find(item => item.id === +id);
+        if (!coffee) {
+            throw new NotFoundException(`Coffee #${id} not found`);
+            // throw new HttpException(`Coffee #${id} not found`, HttpStatus.NOT_FOUND);
+        }
+        return coffee;
     }
 
     create(createCoffeeDto: any) {
