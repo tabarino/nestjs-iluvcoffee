f63769682a50954c1582601df3b74743f21eb33c
tabarino <tabarino@outlook.com>
2020-12-15 16:48:03 +0000

    Response Status Codes

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -7,11 +7,28 @@ export class CoffeesController {
         return 'this action returns all coffees';
     }
 
+    /**
+     * You can use the response in the same way we do in express
+     * Using @Res() param decorator
+     * It uses underlying platform Response objects (from Express.js or Fastify)
+     * ========================================
+     * AS BEST PRACTICE WE DO NOT USE LIKE THIS
+     * ========================================
+     * It's hard to test and maintain this kind of code
+     */ 
+    // @Get()
+    // findAll(@Res() response) {
+    //     // Express.js example using status() and send() methods
+    //     response.status(200).send('this action returns all coffees');
+    // }
+
     @Get(':id')
     findOne(@Param('id') id: string) {
         return `this action returns #${id} coffee`;
     }
 
+    // You can set return codes using @HttpCode Decorator
+    // @HttpCode(HttpStatus.GONE)
     @Post()
     create(@Body() body) {
         return body;
