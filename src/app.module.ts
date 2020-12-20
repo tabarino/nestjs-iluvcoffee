c8432b8fa6930ab2d8756d8921c21c20465dc9a2
tabarino <tabarino@outlook.com>
2020-12-20 17:37:00 +0000

    Introducing the TypeORM Module with Docker and Postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
     imports: [
-        CoffeesModule
+        CoffeesModule,
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'postgres',
+            password: 'Root1234',
+            database: 'postgres',
+            autoLoadEntities: true,
+            synchronize: true // This option must be disabled on Production
+        })
     ],
     controllers: [
         AppController
