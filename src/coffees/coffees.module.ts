9add2fce40584b1dc9bf0ca73a8a93071da2d263
tabarino <tabarino@outlook.com>
2020-12-24 10:20:33 +0000

    Creating a TypeORM Entity

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeesController } from './controllers/coffees.controller';
+import { Coffee } from './entities/coffee.entity';
 import { CoffeesService } from './services/coffees.service';
 
 @Module({
+    imports: [
+        TypeOrmModule.forFeature([Coffee])
+    ],
     controllers: [
         CoffeesController
     ],
--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,18 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+// sql table => coffees
+// If you do not set the name, it will be the name of the class in lowercase
+@Entity('coffees')
 export class Coffee {
+    @PrimaryGeneratedColumn()
     id: number;
+
+    @Column()
     name: string;
+
+    @Column()
     brand: string;
+
+    @Column('json', { nullable: true })
     flavours: string[];
 }
