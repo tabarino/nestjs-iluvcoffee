51917e4080f62c90604e7a1a623abfd31727d348
tabarino <tabarino@outlook.com>
2020-12-24 17:59:14 +0000

    Adding Pagination

--- src/coffees/services/coffees.service.ts
+++ src/coffees/services/coffees.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { Repository } from 'typeorm';
 import { CreateCoffeeDto } from '../dto/create-coffee.dto';
 import { UpdateCoffeeDto } from '../dto/update-coffee.dto';
@@ -15,9 +16,13 @@ export class CoffeesService {
         private readonly flavourRepository: Repository<Flavour>
     ) { }
 
-    findAll() {
+    findAll(paginationQuery: PaginationQueryDto) {
+        const { limit, offset } = paginationQuery;
+
         return this.coffeeRepository.find({
-            relations: ['flavours']
+            relations: ['flavours'],
+            skip: offset,
+            take: limit
         });
     }
 
