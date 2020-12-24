cbec9765149d426cb75bcb6642b43f91c4db1e58
tabarino <tabarino@outlook.com>
2020-12-24 12:29:53 +0000

    Create a Relation between two Entities

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeesController } from './controllers/coffees.controller';
 import { Coffee } from './entities/coffee.entity';
+import { Flavour } from './entities/flavour.entity';
 import { CoffeesService } from './services/coffees.service';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([Coffee])
+        TypeOrmModule.forFeature([Coffee, Flavour])
     ],
     controllers: [
         CoffeesController
--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Flavour } from './flavour.entity';
 
 // sql table => coffees
 // If you do not set the name, it will be the name of the class in lowercase
@@ -13,6 +14,7 @@ export class Coffee {
     @Column()
     brand: string;
 
-    @Column('json', { nullable: true })
+    @JoinTable()
+    @ManyToMany(type => Flavour, flavour => flavour.coffees)
     flavours: string[];
 }
