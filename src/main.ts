6fee6262e1d98e8622072b2323ab3f23268fcf2d
tabarino <tabarino@outlook.com>
2020-12-26 17:58:51 +0000

    Introducing Swagger Module

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
@@ -20,6 +21,17 @@ async function bootstrap() {
         new WrapResponseInterceptor(),
         new TimeoutInterceptor()
     );
+
+    // http://localhost:3000/api
+    // Setting up Swagger Document
+    const options = new DocumentBuilder()
+        .setTitle('Iluvcoffee')
+        .setDescription('Coffee application')
+        .setVersion('1.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
+
     await app.listen(3000);
 }
 bootstrap();
